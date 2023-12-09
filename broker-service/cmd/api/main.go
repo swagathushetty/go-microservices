de14d061a01530767825a2c8f036ec9b56e8c74d
swagath shetty <swagath.shetty@gmail.com>
2023-12-09 22:55:44 +0530

    finished server

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
+
 func main() {
+
+	app := Config{}
+
+	log.Println("starting broker service on port %s", webPort)
+
+	//define http server
+	srv := &http.Server{
+		Addr: fmt.Sprintf("%s", webPort),
+	}
+
+	//start server
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Panic(err)
+	}
 
 }
